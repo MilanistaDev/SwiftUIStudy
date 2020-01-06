6b1c50f53026c13c1114f7818c8ae62bf4ea32d4
Takuya Aso <milanista224@gmail.com>
2020-01-06 20:16:51 +0900

    東西線ロゴ画像を追加・07Seriesの画像の上に表示

--- SwiftUIStudy/View/TozaiLineStaList/TozaiLineStaListView.swift
+++ SwiftUIStudy/View/TozaiLineStaList/TozaiLineStaListView.swift
@@ -12,14 +12,28 @@ struct TozaiLineStaListView: View {
     var body: some View {
         VStack(alignment: .leading, content: {
             Image("07Series")
-            .resizable()
-            .scaledToFit()
-            Text("駅リスト")
+                .resizable()
+                .scaledToFit()
+            Image("ic_TozaiLine")
+                .resizable()
+                .scaledToFit()
+                .frame(minWidth: 150, idealWidth: 150, maxWidth: 150,
+                       alignment: Alignment.center)
+                .frame(minWidth: 20, maxWidth: .infinity)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.white, lineWidth: 2))
+                .shadow(radius: 10)
+                .offset(y: -80)
+                .padding(.bottom, -80)
+            Text("東京メトロ東西線 駅リスト")
+                .font(.headline)
+                .padding(.leading, 16.0)
             List {
                 Text("dsds")
             }
         })
-        .navigationBarTitle(Text("東京メトロ 東西線"), displayMode: .inline)
+        .navigationBarTitle(Text("東京メトロ東西線"), displayMode: .inline)
     }
 }
 
