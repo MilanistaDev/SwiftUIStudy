efa17c1c4043365ea6918cdd88d7596dcaa682fd
Takuya Aso <milanista224@gmail.com>
2020-04-17 17:14:53 +0900

    ログインフォームサンプル用のViewファイルを追加

---
+++ SwiftUIStudy/View/SecureTextField/SecureTextField.swift
@@ -0,0 +1,21 @@
+//
+//  SecureTextField.swift
+//  SwiftUIStudy
+//
+//  Created by Takuya Aso on 2020/04/17.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct SecureTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecureTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        SecureTextField()
+    }
+}
--- SwiftUIStudy/View/Top/TopListView.swift
+++ SwiftUIStudy/View/Top/TopListView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct TopListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                NavigationLink(destination: SecureTextField()) {
+                    Text("ログインフォームサンプル")
+                }
+            }.navigationBarTitle(Text("SwiftUIStudy Contents"), displayMode: .large)
+        }
     }
 }
 
