6fa1463ca2f2ad67ec976eaf7b9fc09b9772228e
Takuya Aso <milanista224@gmail.com>
2020-04-24 16:00:36 +0900

    トップ画面から新しいコンテンツに遷移する部分の実装

---
+++ SwiftUIStudy/View/UseXibInsteadOfView/UseXibInsteadOfView.swift
@@ -0,0 +1,21 @@
+//
+//  UseXibInsteadOfView.swift
+//  SwiftUIStudy
+//
+//  Created by Takuya Aso on 2020/04/24.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct UseXibInsteadOfView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UseXibInsteadOfView_Previews: PreviewProvider {
+    static var previews: some View {
+        UseXibInsteadOfView()
+    }
+}
