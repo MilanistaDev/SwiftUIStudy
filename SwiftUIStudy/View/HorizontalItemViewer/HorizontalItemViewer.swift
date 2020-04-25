83e1bf0180623a7842b45719d8853c35bebad1c3
Takuya Aso <milanista224@gmail.com>
2020-04-26 02:08:59 +0900

    画像の下に説明文のビューを配置

--- SwiftUIStudy/View/HorizontalItemViewer/HorizontalItemViewer.swift
+++ SwiftUIStudy/View/HorizontalItemViewer/HorizontalItemViewer.swift
@@ -13,22 +13,45 @@ struct HorizontalItemViewer: View {
     private let adjustPadding: CGFloat = 30.0
 
     var body: some View {
-        GeometryReader { geometry in
+        VStack(alignment: .leading) {
+            Text("WWDC19 Dishes")
+                .font(.title)
+                .padding(.leading, 30.0)
+                .padding(.top, 16.0)
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: .center, spacing: 20.0) {
+                HStack(spacing: 20.0) {
                     ForEach(0..<17) { index in
-                        Image("WWDC19_DISH_" + String(index))
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: geometry.size.width - self.adjustPadding * 2, height: (geometry.size.width - self.adjustPadding * 2) * 4 / 3, alignment: .center)
-                            .border(Color.black, width: 0.5)
-                            .clipped()
+                        VStack(alignment: .leading) {
+                            Image("WWDC19_DISH_" + String(index))
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: UIScreen.main.bounds.width - self.adjustPadding * 2, height: (UIScreen.main.bounds.width - self.adjustPadding * 2) * 4 / 3, alignment: .center)
+                                .border(Color.black, width: 0.5)
+                                .clipped()
+                            VStack(alignment: .leading) {
+                                Text("WWDC19 料理紹介")
+                                    .font(.headline)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .leading], 8.0)
+                                Text("WWDC19 Bashで司会をするクレイグ・フェデリギ氏WWDC19 Bashで司会をするクレイグ・フェデリギ氏会をするクレイグ・フェ")
+                                    .font(.footnote)
+                                    .foregroundColor(.black)
+                                    .lineLimit(3)
+                                    .padding(.all, 8.0)
+                            }
+                            .frame(width: UIScreen.main.bounds.width - self.adjustPadding * 2)
+                            .background(Color.yellow)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+
+                        }
+
                     }
                 }
                 .padding([.leading, .trailing], self.adjustPadding)
             }
+            .navigationBarTitle("Horizontal Item Viewer", displayMode: .inline)
+            Spacer()
         }
-        .navigationBarTitle("Horizontal Item Viewer", displayMode: .inline)
     }
 }
 
