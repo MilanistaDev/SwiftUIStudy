979965fe59cec5cfd505f54e8d0f9ed7bca6440e
Takuya Aso <milanista224@gmail.com>
2020-04-26 00:28:04 +0900

    画像を追加・画像を表示させる

--- SwiftUIStudy/View/HorizontalItemViewer/HorizontalItemViewer.swift
+++ SwiftUIStudy/View/HorizontalItemViewer/HorizontalItemViewer.swift
@@ -10,20 +10,22 @@ import SwiftUI
 
 struct HorizontalItemViewer: View {
 
-    var imageNames: [String] = ["T01", "T02", "T03", "T04"]
+    private let adjustPadding: CGFloat = 30.0
 
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .center, spacing: 20.0) {
-                    ForEach(self.imageNames, id: \.self) { imageName in
-                        Image(imageName)
-                            .scaledToFill()
-                            .frame(width: 300.0, height: 400.0)
-                        .border(Color.black, width: 0.5)
+                    ForEach(0..<17) { index in
+                        Image("WWDC19_DISH_" + String(index))
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geometry.size.width - self.adjustPadding * 2, height: (geometry.size.width - self.adjustPadding * 2) * 4 / 3, alignment: .center)
+                            .border(Color.black, width: 0.5)
+                            .clipped()
                     }
                 }
-                .padding([.leading, .trailing], (geometry.size.width - 300) / 2)
+                .padding([.leading, .trailing], self.adjustPadding)
             }
         }
         .navigationBarTitle("Horizontal Item Viewer", displayMode: .inline)
