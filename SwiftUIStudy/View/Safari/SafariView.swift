822446ba6322fd261fc872c45816db4f5b189192
Takuya Aso <milanista224@gmail.com>
2020-04-27 02:13:56 +0900

    SFSafariVCを開くビューを追加

---
+++ SwiftUIStudy/View/Safari/SafariView.swift
@@ -0,0 +1,29 @@
+//
+//  SafariView.swift
+//  SwiftUIStudy
+//
+//  Created by Takuya Aso on 2020/04/27.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = SFSafariViewController
+
+    var url: URL?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url!)
+    }
+
+    func updateUIViewController(_ safariViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
+
+struct SafariView_Previews: PreviewProvider {
+    static var previews: some View {
+        SafariView(url: URL(string: "https://www.google.com/"))
+    }
+}
