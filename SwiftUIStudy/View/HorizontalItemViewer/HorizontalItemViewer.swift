214417b3a013479d302548d328f2f1452f306bf8
Takuya Aso <milanista224@gmail.com>
2020-04-29 21:16:33 +0900

    rotation3DEffectを使ってカルーセルチックな動きに

--- SwiftUIStudy/View/HorizontalItemViewer/HorizontalItemViewer.swift
+++ SwiftUIStudy/View/HorizontalItemViewer/HorizontalItemViewer.swift
@@ -21,48 +21,54 @@ struct HorizontalItemViewer: View {
                 .padding(.leading, 30.0)
                 .padding(.top, 16.0)
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20.0) {
+                HStack(spacing: UIScreen.main.bounds.width * 0.75) {
                     ForEach(0..<wwdcDishes.count) { index in
-                        VStack(alignment: .leading) {
-                            Image("WWDC19_DISH_" + String(index))
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: UIScreen.main.bounds.width - self.adjustPadding * 2, height: (UIScreen.main.bounds.width - self.adjustPadding * 2) * 4 / 3, alignment: .center)
-                                .border(Color.black, width: 0.5)
-                                .clipped()
+                        GeometryReader { geometry in
                             VStack(alignment: .leading) {
-                                HStack {
-                                    Text(wwdcDishes[index].title)
-                                        .font(.headline)
+                                Image("WWDC19_DISH_" + String(index))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: UIScreen.main.bounds.width - self.adjustPadding * 2,
+                                           height: (UIScreen.main.bounds.width - self.adjustPadding * 2) * 4 / 3, alignment: .center)
+                                    .border(Color.black, width: 0.5)
+                                    .clipped()
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        Text(wwdcDishes[index].title)
+                                            .font(.headline)
+                                            .foregroundColor(.black)
+                                        Spacer()
+                                        Button(action: {
+                                            self.showModal.toggle()
+                                            self.selectedNum = index
+                                        }) {
+                                            Image(systemName: "link.circle")
+                                        }
+                                        .frame(width: 16.0, height: 16.0)
+                                        .opacity(wwdcDishes[index].link.isEmpty ? 0.0 : 1.0)
+                                        .sheet(isPresented: self.$showModal) {
+                                            SafariView(url: URL(string: wwdcDishes[self.selectedNum].link))
+                                                .edgesIgnoringSafeArea(.bottom)
+                                        }
+                                    }
+                                    .padding([.top, .horizontal], 8.0)
+                                    Text(wwdcDishes[index].description)
+                                        .font(.footnote)
                                         .foregroundColor(.black)
-                                    Spacer()
-                                    Button(action: {
-                                        self.showModal.toggle()
-                                        self.selectedNum = index
-                                    }) {
-                                        Image(systemName: "link.circle")
-                                    }
-                                    .frame(width: 16.0, height: 16.0)
-                                    .opacity(wwdcDishes[index].link.isEmpty ? 0.0 : 1.0)
-                                    .sheet(isPresented: self.$showModal) {
-                                        SafariView(url: URL(string: wwdcDishes[self.selectedNum].link))
-                                            .edgesIgnoringSafeArea(.bottom)
-                                    }
+                                        .lineLimit(3)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                        .padding([.horizontal, .bottom], 8.0)
                                 }
-                                .padding([.top, .horizontal], 8.0)
-                                Text(wwdcDishes[index].description)
-                                    .font(.footnote)
-                                    .foregroundColor(.black)
-                                    .lineLimit(3)
-                                    .padding([.horizontal, .bottom], 8.0)
+                                .frame(width: UIScreen.main.bounds.width - self.adjustPadding * 2)
+                                .background(Color.yellow)
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
                             }
-                            .frame(width: UIScreen.main.bounds.width - self.adjustPadding * 2)
-                            .background(Color.yellow)
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .rotation3DEffect(Angle(degrees: (Double(geometry.frame(in: .global).minX) - Double(UIScreen.main.bounds.width) / 2 - 5.0) / -10),
+                                              axis: (x: 0.0, y: 1.0, z: 0.0))
                         }
                     }
                 }
-                .padding([.leading, .trailing], self.adjustPadding)
+                .padding(.horizontal, UIScreen.main.bounds.width / 2 - 5.0)
             }
             .navigationBarTitle("Horizontal Item Viewer", displayMode: .inline)
             Spacer()
