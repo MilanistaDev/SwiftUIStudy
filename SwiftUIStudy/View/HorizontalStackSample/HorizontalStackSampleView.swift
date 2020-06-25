6138d1963d5bb6f47215f391a3657b0d633d4187
Takuya Aso <milanista224@gmail.com>
2020-06-25 22:12:08 +0900

    LazyHStackのサンプル用のViewファイルを追加

---
+++ SwiftUIStudy/View/HorizontalStackSample/HorizontalStackSampleView.swift
@@ -0,0 +1,21 @@
+//
+//  HorizontalStackSampleView.swift
+//  SwiftUIStudy
+//
+//  Created by Takuya Aso on 2020/06/25.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct HorizontalStackSampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HorizontalStackSampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        HorizontalStackSampleView()
+    }
+}
